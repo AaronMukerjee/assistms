dbd5ca2c3feec32b81e9590565741839005d9113
AaronMukerjee <amukerjee97@gmail.com>
2022-07-05 13:44:14 +0530

    changes to test file

--- src/test/java/com/portal/src/AssistMsApplicationTests.java
+++ src/test/java/com/portal/src/AssistMsApplicationTests.java
@@ -3,7 +3,7 @@ package com.portal.src;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AssistMsApplicationTests {
 
 	@Test
