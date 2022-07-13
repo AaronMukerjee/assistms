f66647cd71ae4bfec99b9740b7734d645bc6ec1d
Prateek1105 <36219176+Prateek1105@users.noreply.github.com>
2022-07-14 02:11:54 +0530

    Resolved controller advice

--- src/main/java/com/portal/src/exceptionshandling/ExceptionControllerAdvice.java
+++ src/main/java/com/portal/src/exceptionshandling/ExceptionControllerAdvice.java
@@ -20,16 +20,24 @@ public class ExceptionControllerAdvice {
 		return new ResponseEntity<ExceptionResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	@ExceptionHandler(value = {TrainingDataFetchException.class})
-	public ResponseEntity<ExceptionResponse> handleTrainingDataFetchException(IntentNotCreatedException ex){
+	public ResponseEntity<ExceptionResponse> handleTrainingDataFetchException(TrainingDataFetchException ex){
 		ExceptionResponse response = new ExceptionResponse();
 		response.setMessage("The Data could not be fetched.");
 		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 		return new ResponseEntity<ExceptionResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	@ExceptionHandler(value = {TrainingDataNotInsertedException.class})
-	public ResponseEntity<ExceptionResponse> handleTrainingDataInsertionException(IntentNotCreatedException ex){
+	public ResponseEntity<ExceptionResponse> handleTrainingDataInsertionException(TrainingDataNotInsertedException ex){
 		ExceptionResponse response = new ExceptionResponse();
 		response.setMessage("The data could not be inserted");
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<ExceptionResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<ExceptionResponse> handleTrainingDataInsertionException(Exception ex){
+		ExceptionResponse response = new ExceptionResponse();
+		response.setMessage("The generic exception occured");
 		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 		return new ResponseEntity<ExceptionResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
